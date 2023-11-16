3c91fb311e41b131f0ebba364c7ec4f48032de18
Totiruzi <totiruzi@yahoo.com>
2023-11-16 11:49:15 +0300

    Add Babel for backwards compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,41 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    main: path.resolve(__dirname, 'src/app.js')
+    main: path.resolve(__dirname, 'src/app.js'),
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename:'[name].[contenthash].js',
-    clean: true
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: '[name][ext]',
+    clean: true,
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+      watch: true,
+    },
     port: 5001,
     open: true,
     hot: true,
-    watchContentBase: true,
   },
   // loaders
   module: {
     rules: [
-      {test: /\.css$/, use: ['style-loader', 'css-loader']},
-    ]
+      // css
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      // images
+      { test: /\.(svg|ico|png|jpg|gif|webp|jpeg)$/, type: 'asset/resource' },
+      // js for babel
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] },
+        },
+      },
+    ],
   },
 
   // plugins
@@ -32,6 +47,6 @@ module.exports = {
       title: 'just-demo',
       filename: 'index.html',
       template: path.resolve(__dirname, 'src/tempt.html'),
-    })
-  ]
-}
+    }),
+  ],
+};
